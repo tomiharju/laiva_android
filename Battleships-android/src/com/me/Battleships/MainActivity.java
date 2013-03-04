52e70e031591251dc45b652d640e0c9c87dc9114
Tomi <Tomi@80.221.63.93>
2013-03-04 20:42:06 +0200

    Removed gitignore for now

--- Battleships-android/src/com/me/Battleships/MainActivity.java
+++ Battleships-android/src/com/me/Battleships/MainActivity.java
@@ -1,29 +1,41 @@
 package com.me.Battleships;
 
 import Core.Main;
-import Core.NativeConnector;
+import Core.NativeFunctions;
 import android.os.Bundle;
 
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
 
-public class MainActivity extends AndroidApplication implements NativeConnector{
+public class MainActivity extends AndroidApplication implements NativeFunctions{
+	
+	
+	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+      
+        //Open socket here
+        //Connect to server
+        //
+        startGame();
         
-        AndroidApplicationConfiguration cfg = new AndroidApplicationConfiguration();
-        cfg.useGL20 = true;
-        cfg.useCompass =false;
-        cfg.useAccelerometer = false;
-        cfg.useWakelock=true;
-        initialize(new Main(this), cfg);
+        //After everything is finished, startGame()
     }
 
 	@Override
 	public void helloworld() {
 		//Android specific function calls
 		
+	}
+	
+	public void startGame(){
+		AndroidApplicationConfiguration cfg = new AndroidApplicationConfiguration();
+        cfg.useGL20 = true;
+        cfg.useCompass =false;
+        cfg.useAccelerometer = false;
+        cfg.useWakelock=true;
+        initialize(new Main(this), cfg);
 	}
 	
 	
